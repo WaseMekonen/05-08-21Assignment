cf47f65cfc712a5ed736deeb67cf914e595a11c9
MekonenWase <mekonen.wase@gmail.com>
2021-08-05 19:19:53 +0300

    home assignment

---
+++ js/main.js
@@ -0,0 +1,267 @@
+//////// exercise: 1 ///////
+
+// var userInputOne = Number(prompt("insert a nummber:"));
+// var userInputTwo = Number(prompt("insert a nummber:"));
+// var sumOfInput = userInputOne+userInputTwo;
+// if(sumOfInput >= 13){
+//     console.log("Bar-Mitzva");
+// }else if(sumOfInput == 12){
+//     console.log("Bat-Mitzva");
+// }else{
+//     console.log("Kid");
+// }
+
+
+
+////// exercise: 2 //////
+// var count=0
+// while(count<=99){
+//     console.log("yes");
+//     count++
+// }
+
+
+////// exercise: 3 //////
+
+
+// var userNumber = Number(prompt("enter a number"));
+// var countt = 0;
+// while(countt<userNumber){
+//     console.log("No");
+//     countt++
+// }
+
+
+////// exercise: 4 //////
+
+
+// for(i =0; i<100; i++){
+//     console.log("javascript is not not JAVA");
+// }
+
+
+////// exercise: 5 //////
+
+// var userInputName = (prompt("enter your name: "));
+// var userInputNumber = Number(prompt("enter a nummber: "));
+
+// for (i=0; i<userInputNumber; i++){
+//     console.log(userInputName);
+// }
+
+
+////// exercise: 6 //////
+
+// var userInputName = prompt("enter your name:"),
+//     userInputAge = Number(prompt("enter your Age:")),
+//     userInputMail = (prompt("enter your Mail:"));
+
+//     console.log(userInputName , userInputAge, userInputMail);
+
+// var userInputNumber =Number(prompt("enter your lucky number: ")),
+//     temp = userInputName+" "+userInputAge+" "+userInputMail;
+
+// for(var i= 0; i < userInputNumber; i++ && userInputAge>=18){
+//     console.log(temp);
+// }
+
+
+////// exercise: 7 //////
+
+// var userNumber = Number(prompt("insert a number:")),
+// userName = prompt("What is your name"),
+// myName = "wase";
+
+// for(var i=0; i < userNumber; i++){
+//     if(userName == myName){
+//         console.log("Hi boss "+myName);
+//     }else{
+//         console.log("NO ENTRY");
+//     }
+// }
+
+
+////// exercise: 8 //////
+
+// var firstNumber = Number(prompt("insert first number")),
+//     secondNumber = Number(prompt("insert second number"));
+
+// for(var i=0; i< firstNumber; i++){
+//     for(var j=0; j<secondNumber; j++){
+//         console.log(firstNumber * secondNumber);
+//     }
+// }
+
+
+////// exercise: 9 //////
+
+// var arrayOfnum = [23,34,5,65,87];
+//  console.log(arrayOfnum);
+
+
+
+ ////// exercise: 10 //////
+
+//  var carBrands = ["skoda","fiat","shevrolet","nissan","mazda"];
+//  console.log(carBrands);
+
+
+
+  ////// exercise: 11 //////
+
+// var numbers = [32, 87, 67, 50, 23, 14, 11, 96];
+//     evenNumbers = [];
+
+// for(var i=0; i<numbers.length; i++){
+//     if(!(numbers[i]%2)){
+//         evenNumbers.push(numbers[i]);
+//     }
+// }
+// console.log(evenNumbers);
+
+
+////// exercise: 12 //////
+
+// var newArray=[];
+
+// for(var i=0; i<5; i++){
+//     var userInput =Number(prompt("please insert a number"));
+//     newArray.push(userInput);
+// }
+// console.log(newArray);
+
+////// exercise: 13 //////
+
+// var userArray = [];
+// var newUserInput;
+
+// for(var i=0; i<7; i++){
+//     newUserInput = Number(prompt("please insert a number"));
+//     if(newUserInput%2){
+//         userArray.push(newUserInput);
+//     }
+// }
+// console.log(userArray);
+
+
+////// exercise: 14 //////
+
+// var randomArray = ["train",24,"ship",77,"car","plane"];
+//     // newRandomArray=[];
+
+// for(var i= 0; i< randomArray.length; i++){
+//     if(!Number(randomArray[i])){
+//         randomArray[i]="Not-number";
+//     }
+    
+//     // newRandomArray.push(randomArray[i]);
+// }
+// console.log(randomArray);
+
+
+////// exercise: 15 //////
+
+// var userArray = [];
+// counter = 0;  
+// while(counter<10){
+//     var userInput = Number(prompt("please insert a number"));
+//     if(!(userInput%2)){
+//         userArray.push(userInput);
+//         counter++
+//     }
+// }
+// console.log(userArray);
+
+////// exercise: 16 //////
+
+
+// var areaOfRectangle, scopeOfRectangle;
+
+// var counter = true;
+// while(counter){
+//     var userInputWidth = Number(prompt("Enter a widht: ")), userInputHeight = Number(prompt("Enter a Height: "));
+//     if (userInputWidth &&userInputHeight){
+//         areaOfRectangle = userInputHeight* userInputWidth;
+//         scopeOfRectangle = (userInputHeight*2) + (userInputWidth*2)
+//         counter=false;
+//     }else{
+//         alert("insert a valid Height/widht");
+//     }
+// }
+// console.log("the area of the rectangle is: "+areaOfRectangle);
+// console.log("the scope of the rectangle is:" +scopeOfRectangle);
+
+
+////// exercise: 17 //////
+
+// num1 =  Number(prompt("Enter a first number: "));
+// num2 =  Number(prompt("Enter a second number: "));
+// num3 =  Number(prompt("Enter a third number: "));
+
+// if (num1<num2 & num2<num3){
+//     alert("Up-going");
+// }else if(num1>num2 && num2>num3){
+//     alert("Down-going");
+// }else{
+//     alert("not in order");
+// }
+
+
+////// exercise: 18 //////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
